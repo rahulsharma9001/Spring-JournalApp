ca31adb4aa683ab31c872a7803ea362ee2dc1557
rahulsharma9001 <rahul.sharmaa9001@gmail.com>
2025-02-21 11:45:54 +0530

    Initial Commit

---
+++ src/main/java/com/rahulsharma/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,34 @@
+package com.rahulsharma.journalApp.controller;
+
+import com.rahulsharma.journalApp.JournalApplication;
+import com.rahulsharma.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+    //Here we write/made endpoints as method
+
+    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping("/getAll")
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        journalEntries.put(myEntry.getId(), myEntry);
+        return true;
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable Long myId){
+        return journalEntries.get(myId);
+    }
+}
